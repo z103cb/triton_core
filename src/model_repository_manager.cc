9bf40d672c7cb71558148e40d9fc7edc74050a12
Kyle McGill <kmcgill@nvidia.com>
2023-01-20 12:10:18 -0800

    working thread

--- src/model_repository_manager.cc
+++ src/model_repository_manager.cc
@@ -822,8 +822,13 @@ ModelRepositoryManager::ComputeModelConfigDiff(const inference::ModelConfig& old
 
   uint32_t result = 0;
   std::string diff_report;
-  pb_differencer.ReportDifferencesToString(&diff_report);
+  pb_util::MessageDifferencer::StreamReporter pb_reporter;
+  pb_reporter.set_report_modified_aggregates(true);
+  // pb_differencer.ReportDifferencesToString(&diff_report);
+  pb_differencer.ReportDifferencesTo(pb_reporter);
   pb_differencer.Compare(old_config, new_config);
+  // LOG_INFO << "Differences reported by protobuf for model_config: " << diff_report;
+  pb_reporter.Print(diff_report);
   LOG_INFO << "Differences reported by protobuf for model_config: " << diff_report;
 
   diff_report.clear();
